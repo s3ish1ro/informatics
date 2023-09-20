a5a5a75f1fd5a9d1482cacc4374b2f5f2957769c
s3ish1ro <145522258+s3ish1ro@users.noreply.github.com>
2023-09-20 11:11:01 +0500

    сделал задание 1(я крутой)

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,5 +1,8 @@
-import math
-
+import math as m
 def task1():
-  pass
-  
+    x = int(input("Введите значение x "))
+    y = int(input("Введите значение y "))
+    z = int(input("Введите значение z "))
+    a = (abs(x-1)**(1/3) + m.cos(y)) / (m.tan(y) + m.sinh(y))
+    b = m.log(abs(z - 1)**(1/2) + abs(y)**(1/3) / (1 + z**2)**(1/2)) + m.sin(y) * m.sin(x)
+    print(float("{0:.4f}".format(a)),float("{0:.4f}".format(b)))
