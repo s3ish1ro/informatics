00ba143645d5cec174d2a87f7ab3a72f5677a090
s3ish1ro <145522258+s3ish1ro@users.noreply.github.com>
2023-09-26 20:33:41 +0500

    сделал всё!

--- labs/lab1.py
+++ labs/lab1.py
@@ -6,8 +6,9 @@ def task1():
     a = (abs(x-1)**(1/3) + m.cos(y)) / (m.tan(y) + m.sinh(y))
     b = m.log(abs(z - 1)**(1/2) + abs(y)**(1/3) / (1 + z**2)**(1/2)) + m.sin(y) * m.sin(x)
     print(float("{0:.4f}".format(a)),float("{0:.4f}".format(b)))
+
 def task2():
-    x = float(input("Введите значение x"))
+    x = float(input("Введите значение x "))
     a = 2
     b = 1
     c = -1
@@ -15,15 +16,15 @@ def task2():
 
 
 def task3():
-    x = float(input("Введите значение x"))
+    x = float(input("Введите значение x "))
     print(float("{0:.4f}".format(m.tan(x)**2 * abs(m.log(x**2)))))
 
 def task4():
-    print("введите координаты первой вершины")
+    print("введите координаты первой вершины ")
     vershina1 = (float(input()), float(input()))
-    print("введите коориданты второй вершины")
+    print("введите коориданты второй вершины ")
     vershina2 = (float(input()), float(input()))
-    print("введите координаты третьей вершины")
+    print("введите координаты третьей вершины ")
     vershina3 = (float(input()), float(input()))
 
 
@@ -40,15 +41,15 @@ def task4():
 
 def task5():
     G = 6.67 * 10**(-11)
-    mass1 = float(input("введите массу первого тела"))
-    mass2 = float(input("введите массу второго тела"))
-    distance = float(input("введите расстояние между телами"))
+    mass1 = float(input("введите массу первого тела "))
+    mass2 = float(input("введите массу второго тела "))
+    distance = float(input("введите расстояние между телами "))
     F = G * (mass1 * mass2) / distance**2
     print(F)
 
 def task6():
-    storona = float(input("введите длину стороны основания"))
-    height = float(input("введите высоту пирамиды"))
+    storona = float(input("введите длину стороны основания "))
+    height = float(input("введите высоту пирамиды "))
     apofema = ((storona / 2)**2 + height**2)**0.5
     S_main = storona**2
     S_bok = 1/2 * apofema * storona * 4
@@ -56,7 +57,35 @@ def task6():
     print("{0:.4f}".format(S_full))
 
 def task7():
-    x1 = float(input("введите координату первой точки"))
-    x2 = float(input("введите координату второй точки"))
+    x1 = float(input("введите координату первой точки "))
+    x2 = float(input("введите координату второй точки "))
     print("{0:.4f}".format(abs(x2 - x1)))
 
+def task8():
+    boatVelocity = float(input("Введите скорость лодки "))
+    courseVelocity = float(input("Введите скорость течения "))
+    if boatVelocity <= courseVelocity:
+        print("Ошибка! Скорость течения больше скорости лодки")
+    timeRiver = float(input("Введите время движения против течения реки "))
+    timeLake = float(input("Введите время движения по озеру"))
+    if boatVelocity < 1:
+        print("Ошибка! Скорость лодки меньше 1")
+    elif courseVelocity < 1:
+        print("Ошибка! Скорость течения меньше 1")
+    elif timeRiver < 1:
+        print("Ошибка! Время движения против течения реки меньше 1")
+    elif timeLake < 1:
+        print("Ошибка! Время движения по озеру меньше одного ")
+    elif boatVelocity > 100 or courseVelocity > 100 or timeLake > 100 or timeRiver > 100:
+        print("Ошибка! Одна из введённых величин больше 100")
+
+    path = boatVelocity * timeLake + (boatVelocity - courseVelocity) * timeRiver
+    if boatVelocity > 1 and boatVelocity < 100 and courseVelocity > 1 and courseVelocity < 100 \
+        and timeRiver > 1 and timeRiver < 100 and timeLake > 1 and timeLake < 100:
+        print(float("{0:.4f}".format(path)))
+
+def task9():
+    rubles = float(input("Введите сумму в рублях "))
+    commision = float(input("Введите курс Юаня "))
+    yuan = rubles * commision
+    print(float("{0:.3f}".format(yuan)))
