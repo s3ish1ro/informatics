5a69bb96bd97f4f291aab49eb67caf4cf7147a0a
s3ish1ro <145522258+s3ish1ro@users.noreply.github.com>
2023-09-27 11:06:34 +0500

    сделал программу лучше
    сделал вывод с текстом у 1 - 4 задания и правильное сравнение вещественных чисел в пятом

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,24 +1,33 @@
 import math as m
+
+
 def task1():
     x = float(input("Введите значение x "))
     y = float(input("Введите значение y "))
     z = float(input("Введите значение z "))
     a = (abs(x-1)**(1/3) + m.cos(y)) / (m.tan(y) + m.sinh(y))
     b = m.log(abs(z - 1)**(1/2) + abs(y)**(1/3) / (1 + z**2)**(1/2)) + m.sin(y) * m.sin(x)
-    print(float("{0:.4f}".format(a)),float("{0:.4f}".format(b)))
+    print("a =",float("{0:.4f}".format(a)))
+    print("b =",float("{0:.4f}".format(b)))
+
+
 
 def task2():
     x = float(input("Введите значение x "))
     a = 2
     b = 1
     c = -1
-    print(float("{0:.4f}".format(((x**2 + a * x/b) + c * x**2)**(1/2))))
+    function = ((x**2 + a * x/b) + c * x**2)**0.5
+    print("значение функции равно",float("{0:.4f}".format(function)),sep = " ")
 
 
 def task3():
     x = float(input("Введите значение x "))
-    print(float("{0:.4f}".format(m.tan(x)**2 * abs(m.log(x**2)))))
+    function = m.tan(x)**2 * abs(m.log(x**2)
+    print("значение функции равно", float("{0:.4f}".format(function),sep = " ")
 
+
+          
 def task4():
     print("введите координаты первой вершины ")
     vershina1 = (float(input()), float(input()))
@@ -26,7 +35,7 @@ def task4():
     vershina2 = (float(input()), float(input()))
     print("введите координаты третьей вершины ")
     vershina3 = (float(input()), float(input()))
-
+    EPS = 10e-3
 
     storona1 = ((vershina2[0] - vershina1[0])**2 + (vershina2[1] - vershina1[1])**2)**0.5
     storona2 = ((vershina3[0] - vershina1[0])**2 + (vershina3[1] - vershina1[1])**2)**0.5
@@ -34,10 +43,13 @@ def task4():
     diagonale2 = (storona1**2 + 1/2 * diagonale1**2)**0.5
     perimetr = storona1 * 4
     ploshad = 1/2 * diagonale1 * diagonale2
-    if storona1 != storona2:
+    if abs(storona1 - storona2) >= EPS:
         print("Заданная фигура не является ромбом")
     else:
-        print(float("{0:.4f}".format(ploshad)),float("{0:.4f}".format(perimetr)))
+        print("Площадь равна",float("{0:.4f}".format(ploshad)))
+        print("Периметр равен",float("{0:.4f}".format(perimetr)))
+
+           
 
 def task5():
     G = 6.67 * 10**(-11)
@@ -46,6 +58,7 @@ def task5():
     distance = float(input("введите расстояние между телами "))
     F = G * (mass1 * mass2) / distance**2
     print(F)
+
 
 def task6():
     storona = float(input("введите длину стороны основания "))
@@ -56,11 +69,14 @@ def task6():
     S_full = S_main + S_bok
     print("{0:.4f}".format(S_full))
 
+
 def task7():
     x1 = float(input("введите координату первой точки "))
     x2 = float(input("введите координату второй точки "))
     print("{0:.4f}".format(abs(x2 - x1)))
 
+
+					
 def task8():
     boatVelocity = float(input("Введите скорость лодки "))
     courseVelocity = float(input("Введите скорость течения "))
@@ -83,6 +99,7 @@ def task8():
     if boatVelocity > 1 and boatVelocity < 100 and courseVelocity > 1 and courseVelocity < 100 \
         and timeRiver > 1 and timeRiver < 100 and timeLake > 1 and timeLake < 100:
         print(float("{0:.4f}".format(path)))
+
 
 def task9():
     rubles = float(input("Введите сумму в рублях "))
