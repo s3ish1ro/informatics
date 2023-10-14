82af7ce6ce8039812e88c29356d642ab7871aa81
s3ish1ro <145522258+s3ish1ro@users.noreply.github.com>
2023-10-14 11:32:45 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -6,7 +6,7 @@ def task1():
     y = float(input("Введите значение y "))
     z = float(input("Введите значение z "))
     a = (abs(x-1)**(1/3) + m.cos(y)) / (m.tan(y) + m.sinh(y))
-    b = m.log(abs(z - 1)**(1/2) + abs(y)**(1/3) / (1 + z**2)**(1/2)) + m.sin(y) * m.sin(x)
+    b = m.log(abs(z - 1)**(1/2)) + abs(y)**(1/3) / (1 + z**2)**(1/2) + m.sin(y) * m.sin(x)
     print("a =", float("{0:.4f}".format(a)))
     print("b =", float("{0:.4f}".format(b)))
 
