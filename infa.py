51e764b1350490bb31a686fcebbeafface8d0d9e
s3ish1ro <145522258+s3ish1ro@users.noreply.github.com>
2024-02-03 16:16:06 +0500

    практика(осталось только 5 задание)

--- infa.py
+++ infa.py
@@ -4,15 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-
-
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
         self.setWindowTitle('График')
-
-        self.canvas = FigureCanvas(plt.figure())
+        self.fig = plt.figure()
+        self.canvas = FigureCanvas(self.fig)
 
         cental_widget = QWidget()
         layout = QVBoxLayout()
@@ -36,7 +34,10 @@ class MainWindow(QMainWindow):
         self.point_input = QLineEdit('50')
 
         self.clear_button = QPushButton('Очистить график')
-        self.clear_button.clicked.connect(self.clear)
+        self.clear_button.clicked.connect(self.clear_plot)
+
+        self.file_button = QPushButton('Сохранить точки в файл')
+        self.file_button.clicked.connect(self.file_save)
 
         layout.addWidget(self.function_label)
         layout.addWidget(self.function_input)
@@ -47,13 +48,14 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.point_input)
         layout.addWidget(self.plot_button)
         layout.addWidget(self.clear_button)
-    def plot_data(self):
+        layout.addWidget(self.file_button)
 
+
+    def vectors(self):
         try:
             expression = self.function_input.text()
-        except ValueError:
+        except NameError:
             expression = 'x'
-
 
         try:
             range_start = float(self.range_start_input.text())
@@ -69,25 +71,45 @@ class MainWindow(QMainWindow):
 
         x = np.linspace(range_start, range_end, points)
         function = functions['f']
+        try:
+            y = [function(value) for value in x]
+        except NameError:
+            y = [value for value in x]
 
-        y = [function(value) for value in x]
+        return x, y
 
-        plt.plot(x, y)
+    def plot_data(self):
+
+        x, y = self.vectors()
+        axes = plt.subplot()
+        axes.plot(x, y)
         plt.grid(True)
-
         plt.xlabel('x')
         plt.ylabel('y')
 
-        plt.title('График y = ' + expression)
-
         self.centralWidget().layout().itemAt(0).widget().draw()
 
-    def clear(self):
-        self.plt.clf()
+    def clear_plot(self):
+        for ax in self.fig.axes:
+            ax.clear()
+        plt.grid(True)
+        self.canvas.draw()
+
+    def file_save(self):
+        x, y = self.vectors()
+        file = open('dots.txt', 'w')
+        file.write('x     ' + '  ' + 'y\n')
+        for i in range(len(x)):
+            a, b = map(str, (x[i], y[i]))
+            a, b = a[0:6], b[0:6]
+            if len(a) < 6:
+                a += '0' * (6 - len(a))
+            if len(b) < 6:
+                b += '0' * (6 - len(b))
+            file.write(a + '  ' + b + '\n')
 
 
 app = QApplication([])
 main_window = MainWindow()
 main_window.show()
-
 app.exec()
